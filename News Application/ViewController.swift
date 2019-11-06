197a702e9cb1af46ab76940217bba78b7fbf33d3
Simranjeet Singh <kahlonsimran93@gmail.com>
2019-11-05 23:47:24 -0500

    Initial Commit

---
+++ News Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News Application
+//
+//  Created by Simranjeet  Singh on 2019-11-05.
+//  Copyright © 2019 Simranjeet  Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
